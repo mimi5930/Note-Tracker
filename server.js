c33aa1114fe9cc4c086b4a277868ea7be988500b
Mike <mikej.miller440@gmail.com>
2022-02-08 14:32:05 -0600

    added api endpoints

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const db = require('./db/db.json')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,6 +15,14 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+})
+
+app.post('/api/notes', (req, res) => {
+    req.body;
 })
 
 app.listen(PORT, () => {
